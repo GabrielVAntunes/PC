c554bf609e00644f3e8d6fbea564109cb1d4cb67
Gabriel Antunes <162385522+GabrielVAntunes@users.noreply.github.com>
2025-05-02 13:45:16 +0100

    Add files via upload

--- TP'S/Guião 1/G1E1.java
+++ TP'S/Guião 1/G1E1.java
@@ -1,39 +1,40 @@
+public class G1E1 {
+    
+    public static void main(String[] args) throws InterruptedException{
+        
+        final int N = Integer.parseInt(args[0]); // número de threads
+        final int I = Integer.parseInt(args[1]); // até que número cada thread vai imprimir
+        
+        // Criamos um array de threads
+        Thread[] listaT = new Thread[N];
+        
+        // Este ciclo inicializa as threads
+        for (int i = 0; i < N; i++){
+            listaT[i] = new Printer(I);
+            listaT[i].start();
+        }
+
+        // Este ciclo aguarda que as threads acabem e se juntem (fazemos isto para que o programa depois siga normalmente,
+        //neste caso não é muito relevante porque depois disso não há mais código)
+        for (int n = 0; n < N; n++){
+            listaT[n].join();                   // o join espera que as threads acabem
+        }
+    }
+}
+
 class Printer extends Thread {
 
+    // até que número cada Thread vai imprimir
     final int I;
 
-    Printer (int I) {                       // de modo a evitar que o I seja uma variável global e que seja passada por parametro no método run
-        this.I = I;                         //passamo-la como variável local e registamos o valor utilizando um construtor 
+    Printer(int I) {
+        this.I = I;
     }
 
+    // método run (obrigatório), quando a thread for ativada irá executar esta função
     public void run(){
-        for (int i = 1; i <= I; i++){
+        for (int i = 0; i < I; i++){
             System.out.println(i);
         }
     }
 }
-
-public class G1E1 {
-
-    public static void main(String[] args) throws InterruptedException{
-
-        final int N = Integer.parseInt(args[0]);
-        final int I = Integer.parseInt(args[1]);
-
-
-        Thread[] arrayT = new Thread[N];            // Criamos uma lista de Threads para conseguir manter as referencias de forma simples, cada posição
-        for (int n = 0; n < N; n++){                //do array corresponde a uma thread
-            arrayT[n] = new Printer(I);
-            arrayT[n].start();                  // O start inicia a thread
-        }
-       
-        for (int n = 0; n < N; n++){
-            arrayT[n].join();                   // o join espera que as threads acabem
-        }
-
-        System.out.println("FIM");
-    }
-}
-
-// javac A.java (para compilar)
-// java A <N> <I> (para correr)
